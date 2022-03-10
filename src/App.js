ebeb5568f1188ebbd7e820989a576abfe60c6044
silhth <81689538+silhth@users.noreply.github.com>
2022-03-10 19:39:03 +0100

    change title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
     
-      <h1>My first Dashboard using react-chartjs-2</h1>
+      <h1>Precision Agicolture</h1>
       <Graph/>
     </div>
   );
